32be1a1bbfd1814e60fa193ac3d430cd2b454b8b
Capwonderbread <hectcast10@yahoo.com>
2022-04-03 00:54:08 -0700

    added functions to display the current weather and display the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ var getCityName = function(city) {
  
 } 
 
-var coordsInsert = function(response) {
+var coordsInsert = function(response,city) {
   var latCoord = response.coord.lat;
   var lonCoord = response.coord.lon;
   var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latCoord + "&lon=" + lonCoord + "&exclude=minutely.hourly,alerts&units=imperial&appid=289b9adb4aa40c0962e1fa10706b278b"
@@ -40,9 +40,40 @@ var coordsInsert = function(response) {
       console.log(response);
       response.json().then(function(data) {
         console.log(data);
+        displayWeather(data, city);
+        display5day(data,data.daily);
       })
     }
   })
+}
+
+var displayWeather = function(data, city) {
+  var date = data.current.dt;
+  var temp = data.current.temp;
+  var wind = data.current.wind_speed;
+  var humidity = data.current.humidity;
+  var uv = data.current.uvi;
+
+  $("#city-weather")
+  .empty()
+  .append("<h2>"+ city +" "+ date +"</h2><p>Temp: "+ temp +"℉</p><p>Wind: "+ wind +" MPH</p><p>Humidity: "+ humidity +"%</p><p>UV Index: <span class='rounded p-1'>"+ uv +"</span></p>");
+}
+
+var display5day = function(data,daily) {
+  
+  $("#city-forecast").empty()
+  for(let i = 1; i < 6; i++) {
+    var iconIn = daily[i];
+    var date = data.daily[i].dt;
+    var temp = data.daily[i].temp.day;
+    var wind = data.daily[i].wind_speed;
+    var humidity = data.daily[i].humidity;
+    
+    $("#city-forecast")
+    .append("<div class='card bg-dark text-light w-100 mx-1'><div class='p-2'><h3 class='text-center'>"+ date +"</h3><img class='card-img-top w-50 mt-2 mx-auto rounded-circle' src='http://openweathermap.org/img/wn/" + iconIn.weather[0].icon + ".png' alt=''><div class='p-2'><p class='card-text'>Temp: "+ temp +"℉</p><p class='card-text'>Wind: "+ wind +" MPH</p><p class='card-text'>Humidity: "+ humidity +"%</p></div></div>");
+  }
+
+
 }
 
 searchFormEl.addEventListener("submit", citySubmit);
